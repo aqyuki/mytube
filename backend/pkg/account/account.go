019ead84a5e34be2a3b8c053dd0364c62c240602
aqyuki <yksk171118@gmail.com>
2024-01-01 01:23:55 +0900

    feat(account): add methods to update password and username

--- backend/pkg/account/account.go
+++ backend/pkg/account/account.go
@@ -17,6 +17,23 @@ func (a *Account) EqualPassword(password string) bool {
 	return comparePassword(a.PasswordHash, password) == nil
 }
 
+// UpdateUserName updates the user name
+func (a *Account) UpdateUserName(userName string) {
+	a.UserName = userName
+	a.UpdatedAt = time.Now()
+}
+
+// UpdatePassword updates the password
+func (a *Account) UpdatePassword(password string) error {
+	hash, err := encryptPassword(password)
+	if err != nil {
+		return err
+	}
+	a.PasswordHash = hash
+	a.UpdatedAt = time.Now()
+	return nil
+}
+
 // NewAccount creates a new account
 func NewAccount(id, user, password string, createdAt, updatedAt time.Time) (*Account, error) {
 	hashedPassword, err := encryptPassword(password)
