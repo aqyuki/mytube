0155f3f1b7abe0bf78e87f807249d090ea06818c
aqyuki <yksk171118@gmail.com>
2024-01-01 01:06:18 +0900

    test(account): add test for Account methods

---
+++ backend/pkg/account/account_test.go
@@ -0,0 +1,38 @@
+package account
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewAccount(t *testing.T) {
+	t.Parallel()
+
+	user, err := NewAccount("id", "user", "password", time.Now(), time.Now())
+	assert.NoError(t, err, "NewAccount should not return an error")
+	if assert.NotNil(t, user, "NewAccount should return a user") {
+		assert.EqualValues(t, "id", user.ID, "NewAccount should return the correct id")
+		assert.EqualValues(t, "user", user.UserName, "NewAccount should return the correct username")
+		assert.NotEmpty(t, user.PasswordHash, "NewAccount should return a password hash")
+		assert.NotEmpty(t, user.CreatedAt, "NewAccount should return a created at timestamp")
+		assert.NotEmpty(t, user.UpdatedAt, "NewAccount should return an updated at timestamp")
+	}
+}
+
+func TestAccount_EqualPassword(t *testing.T) {
+	t.Parallel()
+
+	t.Run("should return true if the password is correct", func(t *testing.T) {
+		user, err := NewAccount("id", "user", "password", time.Now(), time.Now())
+		assert.NoError(t, err, "NewAccount should not return an error")
+		assert.True(t, user.EqualPassword("password"))
+	})
+
+	t.Run("should return false if the password is incorrect", func(t *testing.T) {
+		user, err := NewAccount("id", "user", "password", time.Now(), time.Now())
+		assert.NoError(t, err, "NewAccount should not return an error")
+		assert.False(t, user.EqualPassword("wrong password"))
+	})
+}
