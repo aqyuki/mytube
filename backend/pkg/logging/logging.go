9729a3ea1d57464ee1345ebbc3680e841721c0f3
aqyuki <yksk171118@gmail.com>
2023-12-31 23:54:11 +0900

    feat(pkg): add function to get logger from Context

--- backend/pkg/logging/logging.go
+++ backend/pkg/logging/logging.go
@@ -1,11 +1,16 @@
 package logging
 
 import (
+	"context"
 	"log/slog"
 	"time"
 
 	"github.com/m-mizutani/clog"
 )
+
+type contextKey string
+
+const loggerKey contextKey = "logger"
 
 // NewLogger creates a new slog.Logger
 func NewLogger() *slog.Logger {
@@ -16,4 +21,20 @@ func NewLogger() *slog.Logger {
 		clog.WithPrinter(clog.LinearPrinter),
 	)
 	return slog.New(handler)
+}
+
+// WithLogger creates a new context with a slog.Logger
+// WithLogger returned panic when ctx is nil
+func WithLogger(ctx context.Context, logger *slog.Logger) context.Context {
+	return context.WithValue(ctx, loggerKey, logger)
+}
+
+// FromContext returns a slog.Logger from context
+// If not exist logger in context, return NewLogger()
+func FromContext(ctx context.Context) *slog.Logger {
+	l, ok := ctx.Value(loggerKey).(*slog.Logger)
+	if ok {
+		return l
+	}
+	return NewLogger()
 }
