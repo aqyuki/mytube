a3d457a24414bba3d77862a119178e6e2ac45566
aqyuki <yksk171118@gmail.com>
2024-01-01 01:23:39 +0900

    test(account): add test for Account methods

--- backend/pkg/account/account_test.go
+++ backend/pkg/account/account_test.go
@@ -36,3 +36,27 @@ func TestAccount_EqualPassword(t *testing.T) {
 		assert.False(t, user.EqualPassword("wrong password"))
 	})
 }
+
+func TestAccount_UpdateUserName(t *testing.T) {
+	t.Parallel()
+
+	t.Run("should update the username", func(t *testing.T) {
+		t.Parallel()
+
+		user, err := NewAccount("id", "user", "password", time.Now(), time.Now())
+		assert.NoError(t, err, "NewAccount should not return an error")
+		user.UpdateUserName("new user")
+		assert.EqualValues(t, "new user", user.UserName, "UpdateUserName should update the username")
+	})
+}
+
+func TestAccount_UpdatePassword(t *testing.T) {
+	t.Parallel()
+
+	user, err := NewAccount("id", "user", "password", time.Now(), time.Now())
+	assert.NoError(t, err, "NewAccount should not return an error")
+
+	err = user.UpdatePassword("new password")
+	assert.NoError(t, err, "UpdatePassword should not return an error")
+	assert.True(t, user.EqualPassword("new password"), "UpdatePassword should update the password")
+}
