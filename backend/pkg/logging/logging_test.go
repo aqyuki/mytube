a7675f92d4c126e64eebdd43c20741cb40a96bc6
aqyuki <yksk171118@gmail.com>
2023-12-31 23:54:27 +0900

    test(pkg): add test for function to use context.Context

--- backend/pkg/logging/logging_test.go
+++ backend/pkg/logging/logging_test.go
@@ -1,6 +1,7 @@
 package logging
 
 import (
+	"context"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -11,4 +12,26 @@ func TestNewLogger(t *testing.T) {
 
 	actual := NewLogger()
 	assert.NotNil(t, actual, "NewLogger should not return nil, but received nil")
+}
+
+func TestContext(t *testing.T) {
+	t.Parallel()
+
+	t.Run("with logger", func(t *testing.T) {
+		t.Parallel()
+
+		ctx := context.Background()
+		logger := NewLogger()
+
+		actual := WithLogger(ctx, logger)
+		assert.NotNil(t, actual, "WithLogger should not return nil, but received nil")
+	})
+
+	t.Run("nil logger", func(t *testing.T) {
+		t.Parallel()
+
+		ctx := context.Background()
+		actual := FromContext(ctx)
+		assert.NotNil(t, actual, "FromContext should not return nil, but received nil")
+	})
 }
