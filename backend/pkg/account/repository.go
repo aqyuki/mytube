0ca302ff16a2da1ced0534e5d424be4b43588f58
aqyuki <yksk171118@gmail.com>
2024-01-01 11:55:41 +0900

    feat(account): add AccountRepository

--- backend/pkg/account/repository.go
+++ backend/pkg/account/repository.go
@@ -1,6 +1,12 @@
 package account
 
-import "context"
+import (
+	"context"
+	"database/sql"
+
+	"github.com/uptrace/bun"
+	"github.com/uptrace/bun/dialect/pgdialect"
+)
 
 type Repository interface {
 	// FetchByUsername fetches an account by its username
@@ -11,4 +17,46 @@ type Repository interface {
 	Update(ctx context.Context, account *Account) error
 	// Delete deletes an account from the repository
 	Delete(ctx context.Context, account *Account) error
+}
+
+type AccountRepository struct {
+	db *bun.DB
+}
+
+// NewAccountRepository creates a new account repository
+func NewAccountRepository(sqlDB *sql.DB) *AccountRepository {
+	db := bun.NewDB(sqlDB, pgdialect.New())
+	return &AccountRepository{
+		db: db,
+	}
+}
+
+// FetchByUsername fetches an account by its username
+func (r *AccountRepository) FetchByUsername(ctx context.Context, userName string) (*Account, error) {
+	var user Account
+	if err := r.db.NewSelect().Model(&user).Where("username = ?", userName).Scan(ctx); err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (r *AccountRepository) Register(ctx context.Context, account *Account) error {
+	if _, err := r.db.NewInsert().Model(account).Exec(ctx); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *AccountRepository) Update(ctx context.Context, account *Account) error {
+	if _, err := r.db.NewUpdate().Model(account).Where("id = ?", account.ID).Exec(ctx); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *AccountRepository) Delete(ctx context.Context, account *Account) error {
+	if _, err := r.db.NewDelete().Model(account).Where("id = ?", account.ID).Exec(ctx); err != nil {
+		return err
+	}
+	return nil
 }
