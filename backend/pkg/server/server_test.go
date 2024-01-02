c88f43359f63925e776b10b3dc7917ad03154961
aqyuki <yksk171118@gmail.com>
2024-01-02 17:03:55 +0900

    test(server)!: update constructor

--- backend/pkg/server/server_test.go
+++ backend/pkg/server/server_test.go
@@ -1,9 +1,11 @@
 package server
 
 import (
+	"context"
 	"testing"
 
 	"github.com/aqyuki/mytube/backend/pkg/account"
+	"github.com/aqyuki/mytube/backend/pkg/logging"
 	"github.com/aqyuki/mytube/backend/pkg/session"
 	"github.com/stretchr/testify/assert"
 )
@@ -16,7 +18,8 @@ func TestNew(t *testing.T) {
 		SessionManager: &session.Manager{},
 	}
 
-	server := New(m)
+	ctx := logging.WithLogger(context.Background(), logging.NewLogger())
+	server := New(ctx, m)
 	assert.EqualValues(t, m.AccountService, server.accountService, "account service should be set")
 	assert.EqualValues(t, m.SessionManager, server.sessionManager, "session manager should be set")
 	assert.NotNil(t, server.server, "server should be set")
